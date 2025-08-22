cd17b37e2c9a858510ba2f7d09aa51e38e33dab4
Levi-Ucc <levi.selvage@owl.ucc.edu>
2025-08-21 20:22:47 -0700

    I kinda like this?

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,62 @@
-import { default as winston, transports, createLogger, Logger } from "winston";
+import {
+  default as winston,
+  transports,
+  createLogger,
+  format,
+  Logger,
+} from "winston";
 
 class MyLogger {
   private logger: Logger;
   constructor() {
     this.logger = createLogger({
-      level: "info",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.errors({ stack: true }),
-        winston.format.simple()
-      ),
-      transports: [new transports.Console()],
+      transports: [
+        new transports.Console({
+          format: format.combine(
+            format.timestamp({ format: "MM-YY - HH:MM:SS" }),
+            format.errors({ stack: true }),
+            format.printf(({ timestamp, level, message }) => {
+              return `${timestamp} ${level}: ${message}`;
+            }),
+            format.colorize({ all: true })
+          ),
+        }),
+        new transports.File({
+          filename: "error.log",
+          level: "error",
+          format: format.combine(
+            format.timestamp({ format: "MM-DD-YY - HH:MM:SS" }),
+            format.errors({ stack: true }),
+            format.printf(({ timestamp, level, message, stack }) => {
+              return `${timestamp} ${level}: ${message}\n${stack ? stack : ""}`;
+            }),
+            format.colorize()
+          ),
+        }),
+        new transports.File({
+          filename: "warn.log",
+          level: "warn",
+          format: format.combine(
+            format.timestamp({ format: "MM-DD-YY - HH:MM:SS" }),
+            format.errors({ stack: true }),
+            format.printf(({ timestamp, level, message, stack }) => {
+              return `${timestamp} ${level}: ${message}\n${stack ? stack : ""}`;
+            }),
+            format.colorize()
+          ),
+        }),
+        new transports.File({
+          filename: "combine.log",
+          format: format.combine(
+            format.timestamp({ format: "MM-DD-YY - HH:MM:SS" }),
+            format.errors({ stack: true }),
+            format.printf(({ timestamp, level, message, stack }) => {
+              return `${timestamp} ${level}: ${message}\n${stack ? stack : ""}`;
+            }),
+            format.colorize({ all: true })
+          ),
+        }),
+      ],
     });
   }
 
@@ -37,12 +83,14 @@ const logger: MyLogger = new MyLogger();
 
 logger.info("Log message test");
 logger.error("Error message test");
-logger.warn("Warning message test");
+logger.warn("Warning message tet");
 logger.debug("Debug message test");
 logger.verbose("Verbose message test");
-logger.silly("Silly message test");
+logger.silly("Silly message test", { Code: 2 });
 logger.log("This is a log message with a custom type/level", {
-  customType: "custom",
+  Code: 1,
 });
 
-throw new Error("This is a test error to check error logging");
+for (const formatOption of Object.keys(winston.format)) {
+  logger.info(formatOption);
+}
